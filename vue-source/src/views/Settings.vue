8edf37c482d506b1dc89a8bddd73eb0e512b91a7
Faisal Khalid <faisalawan.live@gmail.com>
2022-06-17 21:07:37 +0500

    new changes

--- vue-source/src/views/Settings.vue
+++ vue-source/src/views/Settings.vue
@@ -1,5 +1,37 @@
 <template>
   <div class="settings">
     <h1>This is an settings page</h1>
+
+    <div>
+      <label for="numrows">Number of Rows</label>
+      <input type="number" v-model="settings.numrows" name="numrows" min="1" max="5">
+
+      <br />
+    <!-- {{settings.humandate}} -->
+      <label for="humandate">Human date</label>
+      <input type="checkbox" v-model="settings.humandate" name="humandate">
+
+      <br />
+
+      <label for="numrows">Number of Rows</label>
+      <input v-model="settings.numrows" name="numrows" min="1" max="5">
+
+      <br />
+      
+    </div>
   </div>
 </template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    },
+    computed: {
+      settings() {
+        return this.$store.state.Settings;
+      }
+    },
+  };
+</script>

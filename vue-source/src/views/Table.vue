c70a385487de9226b04eb05dcdb5859f9e42fcdb
hassankhan-wise <hassankhan.infinitewise@gmail.com>
2022-06-21 01:06:57 +0500

    ui update

--- vue-source/src/views/Table.vue
+++ vue-source/src/views/Table.vue
@@ -2,20 +2,25 @@
   <div class="table-content">
     <h2 class="sec-title">{{ $store.state.Data?.table?.title }}</h2>
 
-    <table class="table wp-list-table widefat" v-if="table">
-      <thead v-if="table.headers">
-        <tr>
-          <th v-for="item in table.headers" :key="item" v-html="item"></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in table.rows" :key="item">
-          <td v-for="head in table.headers" :key="head">
-            {{ item[head.toLowerCase()] }}
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="responsive-table">
+      <table
+        class="wp-list-table widefat fixed striped table-view-list events"
+        v-if="table"
+      >
+        <thead v-if="table.headers">
+          <tr>
+            <th v-for="item in table.headers" :key="item" v-html="item"></th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in table.rows" :key="item">
+            <td v-for="head in table.headers" :key="head">
+              {{ item[head.toLowerCase()] }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
